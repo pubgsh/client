bc9e582ebcfb4b4abba91aa2380e075e4d65977e
Andre Azzolini <apazzolini@gmail.com>
2018-06-09 11:30:32 -0500

    Apply scale directly on Konva group

--- src/routes/Match/Map/CarePackage.js
+++ src/routes/Match/Map/CarePackage.js
@@ -2,49 +2,46 @@ import React from 'react'
 import { Group, Line, Rect } from 'react-konva'
 import { toScale } from '../../../lib/canvas-math.js'
 
-const CarePackage = ({ mapSize, mapScale, carePackage }) => {
-    if (!carePackage) {
-        return null
-    }
+const RECT_SIZE = 11
 
-    const rectSize = 10 / Math.max(1, mapScale / 1.25)
-    const scaledX = toScale(mapSize, carePackage.x)
-    const scaledY = toScale(mapSize, carePackage.y)
-    const rectX = scaledX - rectSize / 2
-    const rectY = scaledY - rectSize / 2
-    const lineWidth = 1 / Math.max(1, mapScale / 1.4)
+const CarePackage = ({ mapSize, mapScale, carePackage }) => {
+    if (!carePackage) return null
 
     return (
-        <Group>
+        <Group
+            x={toScale(mapSize, carePackage.x)}
+            y={toScale(mapSize, carePackage.y)}
+            scale={{ x: 1 / mapScale, y: 1 / mapScale }}
+        >
             <Rect
-                x={rectX}
-                y={rectY}
+                x={-RECT_SIZE / 2}
+                y={-RECT_SIZE / 2}
                 fill="#A02522"
-                width={rectSize}
-                height={rectSize}
+                width={RECT_SIZE}
+                height={RECT_SIZE}
             />
             <Rect
-                x={rectX}
-                y={rectY}
+                x={-RECT_SIZE / 2}
+                y={-RECT_SIZE / 2}
                 fill="#207CAE"
-                width={rectSize}
-                height={rectSize / 3}
+                width={RECT_SIZE}
+                height={RECT_SIZE / 3}
             />
             <Line
                 points={[
-                    rectX + rectSize / 4, rectY,
-                    rectX + rectSize / 4, rectY + rectSize,
+                    -RECT_SIZE / 5, -RECT_SIZE / 2,
+                    -RECT_SIZE / 5, RECT_SIZE / 2,
                 ]}
-                stroke="black"
-                strokeWidth={lineWidth}
+                stroke="#222"
+                strokeWidth={0.5}
             />
             <Line
                 points={[
-                    rectX + (rectSize - rectSize / 4),
-                    rectY, rectX + (rectSize - rectSize / 4), rectY + rectSize,
+                    RECT_SIZE / 5, -RECT_SIZE / 2,
+                    RECT_SIZE / 5, RECT_SIZE / 2,
                 ]}
-                stroke="black"
-                strokeWidth={lineWidth}
+                stroke="#222"
+                strokeWidth={0.5}
             />
         </Group>
     )
