7ed60c428840dbb902e34e49344764d3a03535c2
Andre Azzolini <apazzolini@gmail.com>
2018-09-15 17:54:50 -0500

    Update about page

--- src/routes/About/index.js
+++ src/routes/About/index.js
@@ -55,6 +55,11 @@ export default () =>
             <section>
                 <SectionHeader>Changelog</SectionHeader>
 
+                <h5>2018-09-15</h5>
+                <ul>
+                    <li>Add loadouts (hover on player on roster) (Credit @patrickgordon)</li>
+                </ul>
+
                 <h5>2018-08-27</h5>
                 <ul>
                     <li>Add rewind button (Credit @patrickgordon)</li>
