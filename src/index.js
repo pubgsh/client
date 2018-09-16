c0a211f05b875b1ea358a15977b4213e2443c4db
Andre Azzolini <apazzolini@gmail.com>
2018-09-15 19:34:24 -0500

    Import CSS from index.js to ensure proper versioning

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloProvider } from 'react-apollo'
+import './main.css'
 
 import Routes from './routes.js'
 
