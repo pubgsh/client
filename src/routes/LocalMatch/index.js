7ae79b4e5c5f5b8b5385ee34290f7eaf6b36e5a0
Álvaro Cuesta <alvaro-cuesta@GitHub>
2018-12-24 14:13:49 +0100

    More descriptive error messages when loading local replay

--- src/routes/LocalMatch/index.js
+++ src/routes/LocalMatch/index.js
@@ -14,45 +14,84 @@ const Message = styled.p`
     text-align: center;
 `
 
+const INITIAL_STATE = {
+    // JSON file
+    loading: true,
+    fileError: false,
+    parseError: false,
+    loadedVersion: null,
+    versionError: false,
+    rawTelemetry: null,
+
+    // Telemetry
+    telemetry: null,
+    telemetryLoaded: false,
+    telemetryError: false,
+    rosters: null,
+    globalState: null,
+}
+
 class LocalMatch extends React.Component {
-    state = {
-        loading: true,
-        error: false,
-        rawTelemetry: null,
-        telemetry: null,
-        telemetryLoaded: false,
-        telemetryError: false,
-        rosters: null,
-        globalState: null,
-    }
+    state = INITIAL_STATE
 
     // -------------------------------------------------------------------------
-    // Telemetry, Lifecycle ----------------------------------------------------
+    // Lifecycle ---------------------------------------------------------------
     // -------------------------------------------------------------------------
 
     componentDidMount() {
+        this.loadLocalReplay()
+    }
+
+    // -------------------------------------------------------------------------
+    // Match, Telemetry --------------------------------------------------------
+    // -------------------------------------------------------------------------
+
+    loadLocalReplay = () => {
         // Read replay JSON from file stored in history state
 
         const reader = new FileReader()
 
         reader.onload = read => {
-            try {
-                const data = JSON.parse(read.target.result)
+            let data
 
+            try {
+                data = JSON.parse(read.target.result)
+            } catch (error) {
+                console.error('Error parsing JSON', error)
+                this.setState({ loading: false, parseError: true })
+                return
+            }
+
+            if (!data.version) {
+                console.error('Missing version in JSON')
+                this.setState({ loading: false, parseError: true })
+                return
+            }
+
+            switch (data.version) {
+            // Replay version 1
+            case 1: {
                 if (!data.playerName || !data.match || !data.rawTelemetry) {
-                    throw new Error('Loaded invalid replay JSON')
+                    console.error('Missing field(s) in JSON')
+                    this.setState({ loading: false, parseError: true })
+                    return
                 }
 
                 this.loadTelemetry(data)
-            } catch (error) {
-                console.error(error)
-                this.setState({ loading: false, error: true })
+                return
+            }
+
+            // Unsupported version
+            default: {
+                console.error(`Unsupported replay version ${data.version}`)
+                this.setState({ loading: false, loadedVersion: data.version, versionError: true })
+            }
             }
         }
 
         reader.onerror = error => {
-            console.error('Error reading replay file', error)
-            this.setState({ loading: false, error: true })
+            console.error('Error reading replay file', error.target.error, error)
+            this.setState({ loading: false, fileError: true })
         }
 
         reader.readAsText(this.props.location.state.file)
@@ -111,7 +150,10 @@ class LocalMatch extends React.Component {
         const {
             // JSON file
             loading,
-            error,
+            fileError,
+            parseError,
+            loadedVersion,
+            versionError,
             playerName,
             match,
             rawTelemetry,
@@ -128,8 +170,16 @@ class LocalMatch extends React.Component {
 
         if (loading) {
             content = <Message>Loading...</Message>
-        } else if (error || telemetryError) {
-            content = <Message>An error occurred :(</Message>
+        } else if (fileError) {
+            content = <Message>Error loading replay file :(</Message>
+        } else if (parseError) {
+            content = <Message>
+                Invalid replay file. Only replays downloaded from pubg.sh are supported.
+            </Message>
+        } else if (versionError) {
+            content = <Message>Unsupported replay version ${loadedVersion} :(</Message>
+        } else if (telemetryError) {
+            content = <Message>Error loading telemetry :(</Message>
         } else if (!telemetryLoaded) {
             content = <Message>Loading telemetry...</Message>
         } else {
