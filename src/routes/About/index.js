2dbc769839dcd46c7a815a1cb42359d3f114bab2
Otsomies <otso@contentunion.net>
2022-07-17 19:12:33 +0300

    added changelog item

--- src/routes/About/index.js
+++ src/routes/About/index.js
@@ -55,6 +55,11 @@ export default () =>
             <section>
                 <SectionHeader>Changelog</SectionHeader>
 
+                <h5>2022-07-17</h5>
+                <ul>
+                    <li>Add P90 support (Credit @Otsomies)</li>
+                </ul>
+
                 <h5>2021-07-09</h5>
                 <ul>
                     <li>Add Taego support (Credit @lovethebomb)</li>
