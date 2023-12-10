3d59a343d5f184405339f925744e9a3f484cf7e9
Lukas Polak <admin@lukaspolak.cz>
2023-12-10 13:16:45 +0100

    Handle the case of players attacking non-player entities

--- src/models/Telemetry.parser.js
+++ src/models/Telemetry.parser.js
@@ -237,6 +237,9 @@ export default function parseTelemetry(matchData, telemetry, focusedPlayerName) 
             }
 
             if (d._T === 'LogPlayerTakeDamage') {
+                // Handle the case where a player attacks other entities than players (e.g. bears, bird cages, etc.)
+                if (d.victim.type.indexOf('user') === -1) return
+
                 incrementPlayerStateVal(d.victim.name, 'health', -d.damage)
                 setNewPlayerLocation(d.victim.name, { x: d.victim.location.x, y: d.victim.location.y })
 
